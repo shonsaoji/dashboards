b8ac8f8dae4efc8ebb1102c147d1641823b1363f
root <root@iamwhiney.com>
2012-12-15 20:50:47 +0000

    removed unnecessary references

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>I'm Whiney</title>
+    <title>Dashboards</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
@@ -25,11 +25,6 @@
     <![endif]-->
 
     <!-- Le fav and touch icons -->
-    <link rel="shortcut icon" href="./bootstrap/ico/w_logo.png">
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="./bootstrap/ico/w_logo.png">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="./bootstrap/ico/w_logo.png">
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="./bootstrap/ico/w_logo.png">
-    <link rel="apple-touch-icon-precomposed" href="./bootstrap/ico/w_logo.png">
   </head>
 
   <body>
